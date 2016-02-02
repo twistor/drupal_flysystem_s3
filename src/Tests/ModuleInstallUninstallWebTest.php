955ebbd2ef41e4f8127e80f84c1e0ee80e017b78
Chris Leppanen <chris.leppanen@gmail.com>
2016-02-02 12:12:14 -0800

    Add simple install/uninstall test.

---
+++ src/Tests/ModuleInstallUninstallWebTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\flysystem_s3\Tests\ModuleInstallUninstallWebTest.
+ */
+
+namespace Drupal\flysystem_s3\Tests;
+
+use Drupal\flysystem\Tests\ModuleInstallUninstallWebTest as Base;
+
+/**
+ * Tests module installation and uninstallation.
+ *
+ * @group flysystem_s3
+ */
+class ModuleInstallUninstallWebTest extends Base {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['flysystem_s3'];
+
+}
