281f1cdd0e01f97685776639db1ef3e4fd78d4a1
git <git@3066367.no-reply.drupal.org>
2016-05-04 23:19:32 -0700

    Issue #2674040 by yobottehg, twistor: MimeType is guessed wrong for Drupal generated css aggregate

--- src/Flysystem/Adapter/S3Adapter.php
+++ src/Flysystem/Adapter/S3Adapter.php
@@ -9,6 +9,9 @@ namespace Drupal\flysystem_s3\Flysystem\Adapter;
 
 use League\Flysystem\AdapterInterface;
 use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Config;
+use League\Flysystem\Util;
+use League\Flysystem\Util\MimeType;
 
 /**
  * Overrides methods so it works with Drupal.
@@ -45,6 +48,32 @@ class S3Adapter extends AwsS3Adapter {
     }
 
     return $metadata;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function upload($path, $body, Config $config) {
+    $key = $this->applyPathPrefix($path);
+    $options = $this->getOptionsFromConfig($config);
+    $acl = isset($options['ACL']) ? $options['ACL'] : 'private';
+
+    if (!isset($options['ContentType'])) {
+      if (is_string($body)) {
+        $options['ContentType'] = Util::guessMimeType($path, $body);
+      }
+      else {
+        $options['ContentType'] = MimeType::detectByFilename($path);
+      }
+    }
+
+    if (!isset($options['ContentLength'])) {
+      $options['ContentLength'] = is_string($body) ? Util::contentSize($body) : Util::getStreamSize($body);
+    }
+
+    $this->s3Client->upload($this->bucket, $key, $body, $acl, ['params' => $options]);
+
+    return $this->normalizeResponse($options, $key);
   }
 
 }
